4b7f9d23ad3f4997221ac84c4c429797b4ea9248
Dennis Nesanoff <unknownorganizer@calendar.google.com>
2018-12-16 14:07:58 +0300

    Initial Commit

---
+++ Light/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Light
+//
+//  Created by Dennis Nesanoff on 16/12/2018.
+//  Copyright © 2018 Dennis Nesanoff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
