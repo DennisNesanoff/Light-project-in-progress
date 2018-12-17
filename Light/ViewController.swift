781a15e1dc3c82da5bdae0861bd02fa210df8d7c
Dennis Nesanoff <unknownorganizer@calendar.google.com>
2018-12-17 08:09:34 +0300

    added light

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -7,28 +7,48 @@
 //
 
 import UIKit
+import AVFoundation // A framework for working with audio-visual resources, cameras, control devices, process audio and configure the interaction with the audio system.
 
 class ViewController: UIViewController {
 
-    var lightOn = false
+    var lightOn = true
     override func viewDidLoad() {
         super.viewDidLoad()
         updateUI()
     }
     
-    @IBAction func buttonPressd(_ sender: UIButton) {
+    @IBAction func buttonPressed(_ sender: UIButton) {
         lightOn = !lightOn
         
         updateUI()
     }
 
     func updateUI() {
-        view.backgroundColor = lightOn ? .white : .black
+        let device = AVCaptureDevice.default(for: AVMediaType.video)
+        
+        if let dev = device, dev.hasTorch {
+            view.backgroundColor = .black
+            do {
+                try dev.lockForConfiguration()
+                dev.torchMode = lightOn ? .on : .off
+                dev.unlockForConfiguration()
+            } catch {
+                print(error)
+            }
+        } else {
+            view.backgroundColor = lightOn ? .white : .black
+        }
+//        view.backgroundColor = lightOn ? .white : .black
 //        if lightOn {
 //            view.backgroundColor = UIColor.white
 //        } else {
 //            view.backgroundColor = .black
 //        }
+    }
+    
+    // variable for hidding status bar
+    override var prefersStatusBarHidden: Bool{
+        return true
     }
 }
 
